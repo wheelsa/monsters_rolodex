eed524b66146f1208194631254081d199dc36d31
Seth Wheeler <wheeler.seth@gmail.com>
2020-03-17 08:42:58 -0600

    Updates

---
+++ precache-manifest.39fc1f3c4d80c36fc6a14d901a2a5249.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1ec7c9cf3f11d37f137b2af2402efd5",
+    "url": "/monsters_rolodex/index.html"
+  },
+  {
+    "revision": "2af2b94f9d5cc6bb41c4",
+    "url": "/monsters_rolodex/static/css/main.03691438.chunk.css"
+  },
+  {
+    "revision": "2b7d9edd948003b96165",
+    "url": "/monsters_rolodex/static/js/2.b6796cae.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monsters_rolodex/static/js/2.b6796cae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2af2b94f9d5cc6bb41c4",
+    "url": "/monsters_rolodex/static/js/main.fde16f5b.chunk.js"
+  },
+  {
+    "revision": "4eda091ba1daeac1b8ca",
+    "url": "/monsters_rolodex/static/js/runtime-main.d56462de.js"
+  }
+]);
